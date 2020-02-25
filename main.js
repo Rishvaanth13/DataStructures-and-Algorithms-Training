604a2152ddd9d3acd9f6c5f16477d95d7c3cb73b
Rishvaanth13 <Rishvaanth.T@ril.com>
2020-02-25 17:14:04 +0000

    Auto formated the code

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-function reverse(str){
-  if(!str|| str.length<2 || typeof str != 'string'){
+function reverse(str) {
+  if (!str || str.length < 2 || typeof str != 'string') {
     Console.log("Thats not a right input!")
-  } 
-  else{
+  }
+  else {
     const backwards = [];//var1
-    const length = str.length-1;//var2
+    const length = str.length - 1;//var2
 
-    for(i=length; i>=0; i--){
+    for (i = length; i >= 0; i--) {
       backwards.push(str[i]);
     }
     return backwards.join('');
-  }  
+  }
 }
 reverse('Finally I did something!!');
